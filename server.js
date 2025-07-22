16606b399e63377206111d6448792ec27109979e
RyunK <lrk9582@gmail.com>
2025-07-22 18:13:27 +0900

    기사단 편성 서버 및 ejs 구현

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/statics')) 
+
+
+var fs = require('fs');
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
-app.get('/', (요청, 응답) => {
-  응답.send('반갑다')
+app.get('/formmake', (요청, 응답) => {
+  fs.readdir('./statics/sources/img/characters', (err, filelist) => {
+    // console.log(err);
+    console.log(filelist);
+    let characterslist = filelist.map(e=>{
+      let t = e.split('_')[0]
+      return {file : e, type: t}
+    })
+    응답.render('form_making.ejs', {characterslist : characterslist})
+  })
 }) 
