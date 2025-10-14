d3c4d18706723412a96f7254ba480e5364f70c41
RyunK <lrk9582@gmail.com>
2025-10-14 19:17:44 +0900

    마이페이지 저장한 편성

--- routes/mypage.js
+++ routes/mypage.js
@@ -7,23 +7,29 @@ const getDatas = require('./getDatas.js')
 
 router.get('/', (req, res) => {
     // res.redirect('/forum/share');
-    res.redirect('/forum/share/?sort=tu');
+    res.redirect('/mypage/formsave');
 
 })
 
-// router.get('/:forumtab', (req, res) => {
-//     res.redirect(`/forum/${req.params.forumtab}`);
-// })
 
-router.get('/:forumtab', async(req, res) => {
-    var sql = `SELECT * FROM CONTENTS_NAME
-                WHERE ENG_NAME= ?`;
-    var [result, fields] = await (await connection).execute(sql, [req.query.content ? req.query.content : 'all']);
-    // console.log(result);
-
+router.get('/formsave', async(req, res) => { 
+    var sql = `SELECT * FROM FORM_STATUS`;
+    var [form_status_list, fields] = await (await connection).execute(sql);
+    
     var sql = `SELECT * FROM CONTENTS_NAME
                 ORDER BY KOR_NAME`;
     var [contents_list, fields] = await (await connection).execute(sql);
+    // console.log(result);
+
+    var sql = `SELECT * FROM CONTENTS_NAME
+                WHERE ENG_NAME= ?`;
+    var [result, fields] = await (await connection).execute(sql, [req.query.content ? req.query.content : 'all']);
+
+    var sql = `SELECT * FROM FORM_STATUS
+                WHERE ID= ?`;
+    var [now_formstatus, fields] = await (await connection).execute(sql, [req.query.form_status ? req.query.form_status : '1']);
+
+    console.log(now_formstatus)
 
     var sql = `SELECT LH.ID, types.ENG_NAME AS 'eng_type', types.KOR_NAME AS 'kor_type', 
                 names.ENG_NAME AS 'eng_name', names.KOR_NAME AS 'kor_name', 
@@ -40,7 +46,7 @@ router.get('/:forumtab', async(req, res) => {
     if(!Array.isArray(filtered_heroes_list)){
         filtered_heroes_list = [filtered_heroes_list];
     }
-
+    
     for(let i=0; i<filtered_heroes_list.length; i++){
         var sql = `SELECT LH.ID, types.ENG_NAME AS 'eng_type', types.KOR_NAME AS 'kor_type', 
                 names.ENG_NAME AS 'eng_name', names.KOR_NAME AS 'kor_name', 
@@ -53,24 +59,23 @@ router.get('/:forumtab', async(req, res) => {
 
         filtered_heroes_list_forrender.push(filtered_hero[0]);
     }
-    
-    
+
     let data = {
         nickname: getDatas.loggedInNickname(req, res),
-        forumtab: req.params.forumtab,
+        form_status_list : form_status_list,
+        contents_list : contents_list,
         sort : req.query.sort? req.query.sort : 'tu',
         filtered_heroes : filtered_heroes_list,
         filtered_heroes_forrender : filtered_heroes_list_forrender,
         content : {
-             kor_name : result? result[0]['kor_name'] : '전체 컨텐츠',
+             kor_name : result? result[0].kor_name : '전체 컨텐츠',
              eng_name : req.query.content? req.query.content : 'all',
             },
-        contents_list : contents_list,
+        form_status : now_formstatus[0],
         hero_list : hero_list,
     }
-    // console.log("filtered_heroes : " + filtered_heroes_list)
 
-    res.render('forum.ejs',  {data : data})
+    res.render('mypage_formsave.ejs',  {data : data})
 
 })
 
--- server.js
+++ server.js
@@ -78,6 +78,7 @@ app.use('/login', require('./routes/login.js'));
 app.use('/mailcheck', require('./routes/mailCheck.js'));
 app.use('/forum', require('./routes/forum.js'));
 app.use('/formmake', require('./routes/formmake.js'));
+app.use('/mypage', require('./routes/mypage.js'));
 
 const getDatas = require('./routes/getDatas.js')
 
