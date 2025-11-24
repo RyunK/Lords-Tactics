90b0bf877ce3f6816d2b626ce7ac0a167d9449dd
RyunK <lrk9582@gmail.com>
2025-11-24 16:50:02 +0900

    공지사항 이미지 수정/삭제 시 aws s3 반영

--- routes/info.js
+++ routes/info.js
@@ -9,7 +9,7 @@ const ejs = require('ejs');
 const path = require('path');
 var appDir = path.dirname(require.main.filename);
 
-const { S3Client } = require('@aws-sdk/client-s3')
+const { S3Client, DeleteObjectCommand } = require('@aws-sdk/client-s3')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
 const s3 = new S3Client({
@@ -148,6 +148,10 @@ router.get('/notice/write/:id', mustAdmin, async(req, res) => {
     var sql = `select * from notice_table where id = ?`
     var [result, fields] = await (await connection).execute(sql, [req.params.id])
 
+    // notice_imgs 꺼내서 주기
+    var sql = `select * from notice_imgs where notice_id = ?`
+    var [imgs, fields] = await (await connection).execute(sql, [req.params.id])
+
     if(result.length <= 0){
       throw new Error("존재하지 않는 글입니다.")
     }
@@ -169,25 +173,101 @@ router.get('/notice/write/:id', mustAdmin, async(req, res) => {
 
 router.post('/insertImage', upload.single('img'), async(req, res) => {
     // console.log(req.file)
-    imgurl = '';
+    var imgurl = '', imgkey = '';
     if(req.file !== undefined) {
-        var imgurl = req.file.location; // router에서 붙인 multer가 반환한 url (aws s3 object url)
-        console.log(req.file.location)
+        imgurl = req.file.location; // router에서 붙인 multer가 반환한 url (aws s3 object url)
+        imgkey = req.file.key;
+        // console.log(req.file.key)
     }
-    res.json(imgurl); // json 형태로 반환해주어야 View에서 처리가 가능하다.
-})
+    res.json({
+      imgurl : imgurl,
+      imgkey : imgkey,
+    }); // json 형태로 반환해주어야 View에서 처리가 가능하다.
+});
 
+router.post('/deleteImage/all', async(req, res) => {
+  // console.log(req.file)
+  const imgs = req.body.imgs;
+  try {
+
+    let filenames = imgs.split("/");
+    filenames.shift();
+    filenames.forEach(async element => {
+      const deleteObjectCommand = new DeleteObjectCommand({
+          Bucket: process.env.AWSBUCKET,  
+          Key: element,
+      });
+
+      await s3.send(deleteObjectCommand);
+    });
+     
+    res.json({
+      status: '200',
+      data : {},
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.json({
+      status: '500',
+      message : err.message,
+    });
+  }
+});
+
+/**
+ * 실제 글에 사용되지 않는 이미지들을 삭제
+ * @param {Array} db_imgs S3 및 Mysql에 저장된 이미지들의 key값 array
+ * @param {Array} html_imgs 실제 글에 해당하는 html 태그에서 뜯어온 key값 array
+ */
+function dumpImgs(db_imgs, html_imgs){
+  // db_imgs와 html_imgs 비교해서 html_imgs에 없는 db_img는 s3에서 삭제
+  db_imgs.forEach(async (element) => {
+    if(!html_imgs.includes(element)){
+      const deleteObjectCommand = new DeleteObjectCommand({
+          Bucket: process.env.AWSBUCKET,  
+          Key: element,
+      });
+
+      await s3.send(deleteObjectCommand);
+
+      // mysql 데이터 삭제
+      var sql = `delete from notice_imgs where img_key = ?`
+      var [result, fields] = await (await connection).execute(sql, [element])
+    }
+  });
+}
 
 router.post('/writeNewNotice', mustAdmin, async(req, res) => {
   try{
     if(req.body.subject.length > 100 ) throw new Error("제목이 너무 깁니다.")
-    else if(req.body.subject.length <= 0) throw new Error("제목을 입력하세요.")
-    else if(req.body.content.length <= 0) throw new Error("내용을 입력하세요.")
+    else if(req.body.subject.length <= 0) throw new Error("제목이 없습니다.")
+    else if(req.body.content.length <= 0) throw new Error("내용이 없습니다.")
+    
+    // aws_imgs 파싱
+    console.log(req.body.aws_imgs);
+    let aws_imgs = req.body.aws_imgs.split("/");
+    aws_imgs.shift();
+
+    let html_imgs = req.body.html_imgs;
+    if(typeof(html_imgs) == 'string') html_imgs = [html_imgs];
+
+    // aws_imgs와 html_imgs 비교해서 html_imgs에 없는 aws_img는 s3에서 삭제
+    dumpImgs(aws_imgs, html_imgs);
+
     // db에 업로드
     var sql = `insert into notice_table (pin, subject, body, upload_datetime)
               value (?, ?, ?, NOW())`
     var [result, fields] = await (await connection).execute(sql, [req.body.pin == 'true'? 1:0, req.body.subject, req.body.content])
     
+    // html_imgs mysql에 업로드
+    var sql = `insert into notice_imgs (notice_id, img_key)
+              value (?, ?)`;
+    let notice_id = result.insertId
+    html_imgs.forEach(async (element) => {
+      var [result, fields] = await (await connection).execute(sql, [notice_id, element])
+    })
+
     res.redirect('/info')
   }catch(e){
     console.log(e)
@@ -209,6 +289,35 @@ router.post('/editnotice/:id', mustAdmin, async(req, res) => {
       throw new Error("존재하지 않는 글입니다.")
     }
 
+    // aws_imgs + mysql에 있는 애들이랑 html_imgs 비교해서 삭제.
+    // aws_imgs 파싱
+    console.log(req.body.aws_imgs);
+    let aws_imgs = req.body.aws_imgs.split("/");
+    aws_imgs.shift();
+
+    var sql = `select img_key from notice_imgs where notice_id = ?`
+    var [notice_imgs, fields] = await (await connection).execute(sql, [req.params.id])
+    notice_imgs.forEach((element) => {
+      aws_imgs.push(element.img_key)
+    })
+
+    let html_imgs = req.body.html_imgs;
+    if(typeof(html_imgs) == 'string') html_imgs = [html_imgs];
+    
+    // DB배열/실제 html배열 받아서 html에 없으면 s3와 mysql에서 모두 삭제해주는 함수
+    dumpImgs(aws_imgs, html_imgs);
+
+    // 그리고 html 이미지들중에 mysql에 없는 이미지들만 추가로 저장하기
+    var sql = `select img_key from notice_imgs where notice_id = ?`
+    var [notice_imgs_after, fields] = await (await connection).execute(sql, [req.params.id])
+    html_imgs.forEach(async (element) => {
+      if(!notice_imgs_after.some(e => e.img_key == element)){
+        var sql = `insert into notice_imgs (notice_id, img_key)
+              value (?, ?)`
+        var [result, fields] = await (await connection).execute(sql, [req.params.id, element])
+      }
+    })
+
     // db 업데이트
     var sql = `update notice_table 
               set pin = ? , subject = ?, body = ?
@@ -226,6 +335,19 @@ router.post('/editnotice/:id', mustAdmin, async(req, res) => {
 router.post('/notice/delete', mustAdmin, async(req, res) => {
   try{
     if(!req.body.id || req.body.id.length <= 0 ) throw new Error("게시물 id를 받아오지 못했습니다.")
+     
+    var sql = `select img_key from notice_imgs where notice_id = ?`
+    var [db_imgs, fields] = await (await connection).execute(sql, [req.body.id])
+
+    db_imgs.forEach(async (element) => {
+      const deleteObjectCommand = new DeleteObjectCommand({
+          Bucket: process.env.AWSBUCKET,  
+          Key: element.img_key,
+      });
+
+      await s3.send(deleteObjectCommand);
+    });
+
     // db에서 삭제
     var sql = `delete from notice_table where id = ?`
     var [result, fields] = await (await connection).execute(sql, [req.body.id])
