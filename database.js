a57bc67ff9d34416d9e5d9afc7c8e689c696c150
RyunK <lrk9582@gmail.com>
2025-12-16 17:43:35 +0900

    mysql2 장기간 미요청시 연결 해제 해결

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createConnection({
-host : process.env.DB_HOST,  
-user : process.env.DB_USER,
-password : process.env.DB_PW,
-database : process.env.DB_NAME,
-port : process.env.DB_PORT
+    host : process.env.DB_HOST,  
+    user : process.env.DB_USER,
+    password : process.env.DB_PW,
+    database : process.env.DB_NAME,
+    port : process.env.DB_PORT,
+    enableKeepAlive: true,
 });
 
 // connection.connect(function(err) {
